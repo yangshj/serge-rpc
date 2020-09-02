44c4ba0b7748a8cf5b656118a4e116a2628a676d
shuaijun.yang <shuaijun.yang@ucarinc.com>
2020-09-02 17:07:20 +0800

    rpc 服务调用

--- netty-rpc-test/src/main/java/com/app/test/server/RpcServerBootstrap2.java
+++ netty-rpc-test/src/main/java/com/app/test/server/RpcServerBootstrap2.java
@@ -9,7 +9,7 @@ public class RpcServerBootstrap2 {
     private static final Logger logger = LoggerFactory.getLogger(RpcServerBootstrap2.class);
 
     public static void main(String[] args) {
-        String serverAddress = "127.0.0.1:18866";
+        String serverAddress = "127.0.0.1:18877";
         String registryAddress = "10.104.104.248:5181,10.104.108.93:5181,10.104.108.98:5181";
         RpcServer rpcServer = new RpcServer(serverAddress, registryAddress);
         HelloService helloService1 = new HelloServiceImpl();
